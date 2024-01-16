a74a1992cdc1c750c0357b6358b4620518190909
Mitsouille <144555106+Mitsouille@users.noreply.github.com>
2024-01-16 15:30:58 +0100

    Update ListeTriee.java

--- ListeTriee.java
+++ ListeTriee.java
@@ -36,7 +36,44 @@ public class Principale{
     private static final int CONTIGUE	       = 1;
     private static final int CHAINEE	       = 2;
     private static final int CHAINEE_PLIBRES   = 3;
-	
+
+	public static long tempListe(int code, String[] tab,char operation,int taille){
+		ListeTriee lt;
+		switch(code){
+			case 1:
+				ListeContigue listeCon = new ListeContigue(taille);
+				lt = new ListeTriee(listeCon);
+				break;
+			case 2:
+				ListeChainee listeChainee = new ListeChainee(taille);
+				lt = new ListeTriee(listeChainee);
+				break;
+			default:
+				ListeChaineePlacesLibres listeChaineePL = new ListeChaineePlacesLibres(taille);
+				lt = new ListeTriee(listeChaineePL);
+		}
+		remplir_liste(lt,"noms10000.txt");
+		long date_debut;
+		long date_fin;
+		if(operation == '-'){
+			date_debut = System.nanoTime();
+			for(int i = 0; i < 10; i++){
+				lt.suplisT(tab[i]);
+			}
+			date_fin = System.nanoTime() ;
+		}else{
+			date_debut = System.nanoTime();
+			for(int i = 0; i < 10; i++){
+				lt.adjlisT(tab[i]);
+			}
+			date_fin = System.nanoTime() ;
+
+		}
+        return date_fin - date_debut;
+	}
+
+
+
     // Exemple d'utilisation de LectureFichier et remplissage d'une liste
     public static void remplir_liste(ListeTriee liste, String nom_fichier){
 	LectureFichier lf = new LectureFichier(nom_fichier);
@@ -53,28 +90,40 @@ public class Principale{
 	//Exemple d'utilisation de la classe EcritureFichier
 	EcritureFichier fichier = new EcritureFichier("resultats.csv");
 	fichier.ouvrirFichier();
-	fichier.ecrireLigne("liste;operation;emplacement;duree");
+	fichier.ecrireLigne("type liste;operation;emplacement;duree");
 	fichier.fermerFichier();
 
 	//###################################################################
-// Ouverture du fichier
-		LectureFichier lf = new LectureFichier("noms10000.txt");
+		// Ouverture du fichier
+		//LectureFichier lf = new LectureFichier("noms10000.txt");
 		// lecture du fichier dans un tableau de String
-		String[] tab = lf.lireFichier();
+		//String[] tab = lf.lireFichier();
 		// creation d'une liste chainee
-		ListeChainee lc = new ListeChainee(10000);
+		//ListeChainee lc = new ListeChainee(10000);
 		// creation d'une liste triee
-		ListeTriee lt = new ListeTriee(lc);
+		//ListeTriee lt = new ListeTriee(lc);
 		// ajout des lignes du fichier a la liste
 
-		for (int i = 0; i < tab.length; i ++) {
-			lt.adjlisT(tab[i]);
-		}
-		// demonstration
-		System.out.println(lt.toString());
-		// suppression d'un mot de la liste
-		lt.suplisT("DROUVOT");
-		// demonstration
-		System.out.println(lt.toString());
-    }
+		//Question 7
+		long resultat = tempListe(1,ELEMENTS_DE_DEBUT,'+',10010);
+		System.out.println(resultat);
+		System.out.println(tempListe(2,ELEMENTS_DE_DEBUT,'+',10010));
+		System.out.println(tempListe(3,ELEMENTS_DE_DEBUT,'+',10010));
+
+		//Question 8
+		System.out.println(tempListe(1,ELEMENTS_DE_FIN,'+',10010));
+		System.out.println(tempListe(2,ELEMENTS_DE_FIN,'+',10010));
+		System.out.println(tempListe(3,ELEMENTS_DE_FIN,'+',10010));
+
+		//Question 9
+		System.out.println(tempListe(1,ELEMENTS_DE_DEBUT,'-',10010));
+		System.out.println(tempListe(2,ELEMENTS_DE_DEBUT,'-',10010));
+		System.out.println(tempListe(3,ELEMENTS_DE_DEBUT,'-',10010));
+
+		//Question 10
+		System.out.println(tempListe(1,ELEMENTS_DE_FIN,'-',10010));
+		System.out.println(tempListe(2,ELEMENTS_DE_FIN,'-',10010));
+		System.out.println(tempListe(3,ELEMENTS_DE_FIN,'-',10010));
+
+	}
 }
